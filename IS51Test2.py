a7dde7967e990c1d6736bdaf1ef8d988110810b1
Prot0type1 <solorio011@hotmail.com>
2020-12-03 17:28:53 -0800

    second commit

---
+++ IS51Test2.py
@@ -0,0 +1,15 @@
+"""
+Three catagories of grades will be displayed by this program. These will be the 
+number of grades, average of the grades and the percentages of grades. All three categories will be calulated by 
+retriving the students grades first.
+
+Then the program will calculate the percentage of grades above averaging them.
+
+This will display the number of grades & the 
+average grades & the percentages of grades that are above the average.
+
+"""
+
+"""
+
+"""
